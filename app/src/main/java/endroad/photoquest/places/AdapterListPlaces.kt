7977fea89896122e5b4302375deb60c8704af1dd
RasM24 <dvolyatik@yandex.ru>
2019-12-13 22:13:08 +0700

    [kotlin] Исправил баг в AdapterListPlaces

--- app/src/main/java/endroad/photoquest/places/AdapterListPlaces.kt
+++ app/src/main/java/endroad/photoquest/places/AdapterListPlaces.kt
@@ -1,6 +1,8 @@
 package endroad.photoquest.places
 
+import android.content.Context.*
 import android.graphics.Color
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
@@ -16,10 +18,13 @@ class AdapterListPlaces internal constructor(private val listPlacesActivityActiv
 	private val colorOpenedPlace = Color.argb(200, 180, 180, 180)
 	private val colorDontOpenedPlace = Color.argb(200, 255, 255, 255)
 
-	override fun getView(position: Int, root: View, parent: ViewGroup): View {
+	override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+		val inflater = parent.context.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+		val view = convertView ?: inflater.inflate(R.layout.item, parent, false)
+
 		val distance = sqrt((listPlacesActivityActivity.x - getItem(position).posX).pow(2.0) + (listPlacesActivityActivity.y - getItem(position).posY).pow(2.0))
 
-		return root.apply {
+		return view.apply {
 			if (getItem(position).opened)
 				setBackgroundColor(colorOpenedPlace)
 			else
@@ -29,7 +34,6 @@ class AdapterListPlaces internal constructor(private val listPlacesActivityActiv
 			findViewById<TextView>(R.id.tvPlaceAbout).text = getItem(position).nameDiff()
 			findViewById<ImageView>(R.id.ivPlace).setImageResource(getItem(position).getArea())
 			findViewById<TextView>(R.id.tvPlace).text = getItem(position).name()
-
 		}
 	}
 
